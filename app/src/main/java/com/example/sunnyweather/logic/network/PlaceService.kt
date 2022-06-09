f4e5c471fd70808a00458b2d141c4494192fd297
neimengwuyanzu <meicuowoshizhangyu@gmail.com>
2022-06-10 01:27:20 +0800

    看到了15.4.1

---
+++ app/src/main/java/com/example/sunnyweather/logic/network/PlaceService.kt
@@ -0,0 +1,13 @@
+package com.example.sunnyweather.logic.network
+
+import com.example.sunnyweather.SunnyWeatherApplication
+import com.example.sunnyweather.logic.model.PlaceResponse
+import retrofit2.Call
+import retrofit2.http.GET
+import retrofit2.http.Query
+
+interface PlaceService {
+
+    @GET("v2/place?token=${SunnyWeatherApplication.TOKEN}&lang=zh_CN")
+    fun searchPlaces(@Query("query") query: String) : Call<PlaceResponse>
+}
